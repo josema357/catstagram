23fe135a9620a4b8c4b138e23bdf124971ded870
Jose Manuel <j.ochoa317@gmail.com>
2024-04-23 12:26:07 -0500

    create | login-layout | create login page and layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,28 @@
 <svelte:head>
 	<title>Catstagram</title>
 </svelte:head>
+<script>
+  function redirect(){
+    window.location.href="/app/home";
+  }
+</script>
 <div class="main">
-  <div>
-    <section>Catstagram</section>
+  <div class="box">
+    <h1>Catstagram</h1>
     <section>
-      <div>
-        <input type="text"/>
-        <input type="password"/>
-        <button>Log in</button>
+      <div class="box-input">
+        <input type="text" placeholder="Phone number, username or email"/>
+        <input type="password" placeholder="Password"/>
+        <button on:click={redirect}>Click here</button>
       </div>
-      <p>________ o __________</p>
-      <div>
+      <p class="separator">or</p>
+      <div class="box-recovery">
         <p>Sign in with Facebook</p>
-        <p>Did you forget your password?</p>
+        <p><a href="/">Did you forget your password?</a></p>
       </div>
     </section>
   </div>
-  <div>
+  <div id="box-signup" class="box">
     <p>Don't have an account? <a href="/">Sign up</a></p>
   </div>
   <footer>
@@ -31,6 +36,102 @@
 </div>
 
 <style>
-  
+  .main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100dvh;
+    position: relative;
+    margin: 0 8px;
+    box-sizing: border-box;
+  }
+  .box{
+    border: 1px solid rgb(174, 174, 174);
+    border-radius: 3px;
+    width: 100%;
+    max-width: 350px;
+    padding: 40px 40px 10px;
+    box-sizing: border-box;
+    margin-bottom: 10px;
+  }
+  h1{
+    font-size: 30px;
+    font-weight: 800;
+    text-align: center;
+    margin-bottom: 30px;
+  }
+  .box-input{
+    display: flex;
+    flex-direction: column;
+  }
+  input{
+    padding: 10px;
+    font-size: 12px;
+    border: 1px solid rgb(174, 174, 174);
+    outline: none;
+    border-radius: 3px;
+    margin-bottom: 10px;
+    background-color: rgb(248, 248, 248);
+  }
+  button{
+    padding: 10px;
+    background-color: rgb(66, 183, 42);
+    border: none;
+    border-radius: 5px;
+    color: white;
+    font-weight: 600;
+    cursor: pointer;
+  }
+  .separator{
+    text-align: center;
+    color: rgb(174, 174, 174);
+  }
+  .box-recovery{
+    text-align: center;
+    font-size: 12px;
+    font-weight: 600;
+    color: rgb(56, 81, 133);
+  }
+  .box-recovery > p{
+    cursor: pointer;
+  }
+  .box-recovery a{
+    text-decoration: none;
+    color: rgb(65, 62, 101);
+    font-weight: 400;
+  }
+  #box-signup{
+    padding: 0 40px;
+    text-align: center;
+  }
+  #box-signup > p{
+    font-size: 12px;
+  }
+  #box-signup a{
+    text-decoration: none;
+    font-weight: 600;
+    color: rgb(66, 183, 42)
+  }
+  footer{
+    position: absolute;
+    bottom: 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 10px;
+    font-size: 14px;
+    font-weight: 400;
+    color: rgb(174, 174, 174);
+    margin-bottom: 8px;
+  }
+  footer > span{
+    cursor: pointer;
+  }
+  @media(max-width:260px){
+    .box{
+      padding: 40px 0px 10px;
+    }
+  }
 </style>
 
---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,64 @@
+<script>
+  import heart from '$lib/assets/heart.svg';
+  import search from '$lib/assets/search.svg';
+</script>
+
+<nav>
+  <div>
+    <p>Catstagram</p>
+  </div>
+  <div class="left-side">
+    <div class="search">
+      <img src={search} alt="search-logo">
+      <input type="text" placeholder="Search">
+    </div>
+    <img src={heart} alt="notification-logo">
+  </div>
+</nav>
+<div class="layout">
+  <div>Navbar Lateral</div>
+  <slot></slot>
+</div>
+
+<style>
+  nav{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px;
+    border-bottom: 1px solid rgb(209, 209, 209);
+  }
+  nav p{
+    margin: 0;
+    font-size: 20px;
+    font-weight: 800;
+  }
+  .left-side{
+    display: flex;
+    gap: 15px;
+  }
+  .left-side > img{
+    cursor: pointer;
+  }
+  .search{
+    background-color: #efefef;
+    padding: 8px 10px;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+  input{
+    border: none;
+    outline: none;
+    background-color: transparent;
+  }
+  .layout{
+    display: flex;
+  }
+  @media(max-width:400px){
+    nav p{
+      display: none;
+    }
+  }
+</style>
